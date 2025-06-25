810faac354d48e1495dae31bbd4fe02f3ba6beab
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-06-25 23:53:04 +0530

    html templates added

--- microtweets/tweets/urls.py
+++ microtweets/tweets/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('view', views.get_tweets),
+    path('save', views.save_tweets),
+    path('edit/{tweet_id}', views.tweet_edit),
+    path('delete/{tweet_id}', views.tweet_delete),
 ]
